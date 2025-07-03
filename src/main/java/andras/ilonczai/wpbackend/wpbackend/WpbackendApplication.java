c7a227d7d752d7185a12cff3f09c8fb56a4e703c
Ilonczai András <ilonczai.andras16@gmail.com>
2025-07-03 13:38:49 +0200

    Initialize Spring Boot project with Maven wrapper, configuration files, and basic application structure

---
+++ src/main/java/andras/ilonczai/wpbackend/wpbackend/WpbackendApplication.java
@@ -0,0 +1,13 @@
+package andras.ilonczai.wpbackend.wpbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WpbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WpbackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/andras/ilonczai/wpbackend/wpbackend/WpbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package andras.ilonczai.wpbackend.wpbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WpbackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
